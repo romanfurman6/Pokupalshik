2ade403fcdf7dcdd92925a443de0c6093b80d5a3
Roman Furman <roman.furman@uptech.team>
2016-12-01 11:53:46 +0200

    delete: TabBarCoord

--- Pokupalshik/TabBarCoordinator.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TabBarCoordinator.swift
-//  Pokupalshik
-//
-//  Created by Roman Dmitrieivich on 11/28/16.
-//  Copyright © 2016 Roman Dmitrieivich. All rights reserved.
-//
-
-import Foundation
